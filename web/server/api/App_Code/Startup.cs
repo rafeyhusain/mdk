c0eb95bc8e5a9f9cca6e6ec7eeb57abec2b34428
Syed Rafey Husain <rafey.husain@gmail.com>
2018-09-17 19:18:05 +0500

    Web API Core removed

---
+++ web/server/api/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+        }
+    }
+}
