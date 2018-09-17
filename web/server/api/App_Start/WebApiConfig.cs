e072c36e1bcdd5daf026f89cc3179d5ee170335c
Syed Rafey Husain <rafey.husain@gmail.com>
2018-09-17 21:35:24 +0500

    CarService working

--- web/server/api/App_Start/WebApiConfig.cs
+++ web/server/api/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace api
 {
@@ -10,6 +13,7 @@ namespace api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute(origins: "*", headers: "*", methods: "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ web/server/api/Controllers/CarsController.cs
@@ -0,0 +1,59 @@
+﻿using EntityFrameworkPaginate;
+using model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class CarsController : ApiController
+    {
+        CarDb db = new CarDb();
+
+        public static HttpResponseMessage CreateJsonResponse(object value)
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(value), System.Text.Encoding.UTF8, "application/json")
+            };
+
+        }
+
+        [HttpGet]
+        [Route("api/cars")]
+        public HttpResponseMessage Get()
+        {
+            var filters = new SerachFilters();
+
+            filters.CurrentPage = 1;
+            filters.PageSize = 10;
+            filters.SortBy = 1;
+
+            var list = db.GetCars(filters);
+
+            return CreateJsonResponse(list);
+        }
+
+        [HttpPost]
+        [Route("api/cars/search")]
+        public HttpResponseMessage Search(SerachFilters filters)
+        {
+            if (filters == null)
+            {
+                filters = new SerachFilters();
+
+                filters.CurrentPage = 1;
+                filters.PageSize = 10;
+                filters.SortBy = 1;
+            }
+
+            var list = db.GetCars(filters);
+
+            return CreateJsonResponse(list);
+        }
+    }
+}
